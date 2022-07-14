85942b0cffc706c128681c247f7840bb82a70ac2
Tobias Queck <tobias.queck@sap.com>
2022-07-14 13:12:34 +0200

    generated app

---
+++ ui5con.demo.travel/uimodule/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,11 @@
+/* global QUnit */
+
+QUnit.config.autostart = false;
+
+sap.ui.getCore().attachInit(function () {
+    "use strict";
+
+    sap.ui.require(["ui5con/demo/travel/test/integration/AllJourneys"], function () {
+        QUnit.start();
+    });
+});
